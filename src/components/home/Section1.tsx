ba072f2dbbe6de95c2de5b08112603b7aebcd8eb
Pranjal Bansal <pranjal@manufacanalytics.com>
2024-08-27 20:27:15 +0530

    refactor: minor

--- src/components/home/Section1.tsx
+++ src/components/home/Section1.tsx
@@ -37,7 +37,7 @@ const HomeSection1 = ({ id }: Readonly<{ id: string }>) => {
               <p className="text-base/normal mt-8 drop_in">
                 <Balancer>
                   Welcome to my portfolio! I am a passionate full-stack
-                  developer with 1+ years of experience. I specialize in
+                  developer with 2 years of experience. I specialize in
                   creating user-centric software, proficient in both front-end
                   and back-end development across various platforms. I integrate
                   cutting-edge technology seamlessly while prioritizing design.
--- src/data/experiences.ts
+++ src/data/experiences.ts
@@ -2,39 +2,28 @@ import { IExperienceItem } from "@/types";
 
 const experiences: IExperienceItem[] = [
   {
-    designation: "Front-end Developer",
-    company: "Freelancing",
-    startDate: "July 2022",
-    endDate: "November 2022",
-    isCurrentJob: false,
-    location: "Remote",
-    shortDescription:
-    "I had the privilege of spearheading the development of a travel agency website from inception, where I defined requirements, identified target audiences, and more" ,   
-    description:
-    "As a freelance Frontend Developer, I led the creation of a travel agency website from inception, meticulously defining requirements, workflows, and user experiences. From conceptualization to execution, I orchestrated the development process, ensuring seamless integration of frontend functionalities and captivating design elements. By collaborating closely with stakeholders, I translated their vision into a user-centric platform, optimized for showcasing travel destinations and facilitating bookings. Leveraging modern web technologies and adhering to industry best practices, I crafted an intuitive interface that captivated visitors and drove engagement. Through continuous iteration and refinement, I delivered a polished end product that exceeded client expectations, setting the stage for immersive travel experiences. My tenure as a freelancer on this project showcases my proficiency in frontend development, project management, and my ability to deliver impactful solutions tailored to the travel industry's unique needs."  },
-  {
-    designation: "Front-end Developer",
-    company: "Asteria Aerospace Limited",
-    startDate: "January 2023",
-    endDate: "November 2023",
-    isCurrentJob: false,
-    location: "Remote",
-    shortDescription:
-    "In my recent role, I spearheaded customer-centric innovation and revolutionized data visualization within the organization.I lead the development of key features to enhance the data visualization experience,",
-    description:
-    "In my recent role, I spearheaded customer-centric innovation and revolutionized data visualization within the organization. I lead the development of key features to enhance the data visualization experience, such as enabling users to refocus on drones, introducing a drone trial option for user testing, offering enhanced control over drone heading, and creating a system to mark drone positions in video bookmarks. I resolved issues related to choppy drone movement, resulting in a smoother user experience. "
-   },
-  {
-    designation: "Full Stack Developer",
-    company: "Safar",
+    designation: "Software Development Engineer",
+    company: "Manufac Analytics",
     startDate: "January 2024",
     endDate: "",
     isCurrentJob: true,
     location: "Remote",
     shortDescription:
-    "I had the privilege of leading the development of an ed-tech portal for an early-stage startup, from scratch. I finalized the product flow, ensuring a seamless user experience",
+    "In my recent role, I had the opportunity to integrate AWS Bedrock’s Sonnet 3 model with Deep-Chat, lead key feature development in an annotations platform, and optimize UI components. I also focused on resolving DOM errors, designing schema structures, and conducting code refactoring and PR reviews, all while fostering team collaboration.",
     description:
-    "In spearheading the development of an ed-tech portal for an early-stage startup, I meticulously crafted a Work Breakdown Structure (WBS) to delineate the project's scope and tasks. Taking charge of workflow determination, I finalized the product flow, ensuring a seamless user experience. Collaborating closely with a UX designer, I contributed to defining the portal's look and feel, aligning it with the company's vision and user expectations. Guiding developers through the process, I ensured they were development-ready, providing clear direction and support. From conceptualization to execution, my role encompassed not only strategic planning but also hands-on guidance, fostering a collaborative environment conducive to innovation and excellence. This holistic approach enabled the creation of an ed-tech platform offering diverse courses essential for the 21st century, positioning the startup for success in the competitive digital education landscape."  },
+    "In my recent role, I conducted comprehensive research and successfully integrated AWS Bedrock’s Sonnet 3 model with Deep-Chat, significantly enhancing the ML-Viz platform's capabilities in real-time data visualization and user interaction. I led the development and integration of key features in an annotations platform, including the implementation of the reply editor, resolving complex DOM errors to boost platform stability and performance, and spearheading UI enhancements to elevate user experience and visual consistency. My work also involved designing complex schema structures to optimize data organization and retrieval, and optimizing UI components for improved user interaction and streamlined workflows. Additionally, I performed extensive code refactoring across multiple projects, ensuring high code quality and maintainability, and conducted thorough PR reviews to enforce best practices and coding standards. My proactive issue resolution and facilitation of seamless team collaboration were instrumental in driving project success and fostering a collaborative development environment."  },
+    {
+       designation: "Front-end Developer",
+       company: "Asteria Aerospace Limited",
+       startDate: "July 2022",
+       endDate: "November 2023",
+       isCurrentJob: false,
+       location: "Remote",
+       shortDescription:
+       "I spearheaded customer-centric innovation and revolutionized data visualization within the organization.I lead the development of key features to enhance the data visualization experience,",
+       description:
+       "I spearheaded customer-centric innovation and revolutionized data visualization within the organization. I lead the development of key features to enhance the data visualization experience, such as enabling users to refocus on drones, introducing a drone trial option for user testing, offering enhanced control over drone heading, and creating a system to mark drone positions in video bookmarks. I resolved issues related to choppy drone movement, resulting in a smoother user experience. "
+      },
 ];
 
 export default experiences;
